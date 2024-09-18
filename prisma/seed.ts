7d9d69d2ccba6b9ea97911b217d2a2ddc2ea591a
haqi111 <asidiqibaihaqi@gmail.com>
2024-09-18 09:44:00 +0700

    second commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,14 +9,14 @@ async function main() {
   // Create a user with the provided data
   const user = await prisma.users.create({
     data: {
-      nra: '1/UKM_IK/XXVII/2024',
+      nra: '001/UKM_IK/XXVII/2024',
       nama: 'SuperAdmin',
-      nim: '00000000',
+      nim: '00000001',
       no_telp: '081252055236',
       email: 'admin@superadmin.com',
-      password: '$2a$12$0mzqBvZ50KohsHycEROz6unuu00AhLtDnsQv6/iPmJCCk5.tXNfVG',
+      password: '$2a$12$YXMWL2ONvSasM2VrsRRIqumYgYnrMQV2YzK/YAghXvApoKzIC1JvO',
       jenis_kelamin: 'MALE',
-      agama: 'Kristen',
+      agama: 'Islam',
       image: 'admin.jpg',
       fakultas: 'FTI',
       prodi: 'Informatika',
